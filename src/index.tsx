1578db1e61f61944a3013b2f496b9a72376539d1
BorisPad <borisdelord@proton.me>
2024-12-06 21:21:57 +0100

    refactor: update app structure and remove unused Home component

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,31 @@
-import { render } from 'preact';
-import { LocationProvider, Router, Route } from 'preact-iso';
+import { render } from "preact";
+import { LocationProvider, Router, Route } from "preact-iso";
 
-import { Header } from './components/Header.jsx';
-import { Home } from './pages/Home/index.jsx';
-import { NotFound } from './pages/_404.jsx';
-import './style.css';
+import { Header } from "./components/Header.jsx";
+import { Home } from "./pages/Scenes.js";
+import { NotFound } from "./pages/_404.jsx";
+import RenderScene from "./components/RenderScene.js";
 
 export function App() {
-	return (
-		<LocationProvider>
-			<Header />
-			<main>
-				<Router>
-					<Route path="/" component={Home} />
-					<Route default component={NotFound} />
-				</Router>
-			</main>
-		</LocationProvider>
-	);
+  return (
+    <LocationProvider>
+      <div id="app">
+        <Header />
+        <main>
+          <Router>
+            <Route path="/" component={Home} />
+            <Route path="/renderScene" component={RenderScene} />
+            <Route default component={NotFound} />
+          </Router>
+        </main>
+      </div>
+    </LocationProvider>
+  );
 }
 
-render(<App />, document.getElementById('app'));
+const appElement = document.getElementById("app");
+if (appElement) {
+  render(<App />, appElement);
+} else {
+  console.error("App root element not found");
+}
