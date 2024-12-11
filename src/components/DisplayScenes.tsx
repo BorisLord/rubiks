681b4846776308344e4b4ed099086165e26f2b92
BorisPad <borisdelord@proton.me>
2024-12-11 21:46:12 +0100

    update css

--- src/components/DisplayScenes.tsx
+++ src/components/DisplayScenes.tsx
@@ -7,11 +7,11 @@ export default function DisplayScene(): JSX.Element {
 
   return (
     <div className="p-8 bg-white rounded-lg shadow-md max-w-3xl mx-auto">
-      <ul className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+      <ul className="grid grid-cols-2 sm:grid-cols-2 gap-4">
         {sceneNames.map((sceneName) => (
           <li
             key={sceneName}
-            className="text-center border rounded-lg p-4 hover:shadow-lg"
+            className="text-center rounded-sm ring ring-gray-400 p-4 hover:shadow-lg"
           >
             <a
               href={`/renderScene?scene=${sceneName}`}
