be52bccddf9c6ce77c64d62ce57f239e5a810312
BorisPad <borisdelord@proton.me>
2024-12-11 01:26:48 +0100

    add debug layer toggle

--- src/components/RenderScene.tsx
+++ src/components/RenderScene.tsx
@@ -1,16 +1,18 @@
-import { Engine } from "@babylonjs/core";
-import { JSX } from "preact";
+// import "@babylonjs/core/Debug/debugLayer";
+// import "@babylonjs/inspector";
 import { useEffect, useRef } from "preact/hooks";
 import { useLocation } from "preact-iso";
 
+import { JSX } from "preact";
+import { Engine } from "@babylonjs/core";
 import { Scenes } from "../classes/exportScenes";
 
 export default function RenderScene(): JSX.Element {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
   const { query } = useLocation();
   const sceneName = query.scene;
-  console.log("sceneName", sceneName);
-  console.log("CANVA", canvasRef);
+  // console.log("sceneName", sceneName);
+  // console.log("CANVA", canvasRef);
 
   useEffect(() => {
     if (!canvasRef.current) {
@@ -20,15 +22,39 @@ export default function RenderScene(): JSX.Element {
 
     const engine = new Engine(canvasRef.current, true);
 
-    // Charger la scène "First" depuis vos exports
-    const scene = Scenes[sceneName as keyof typeof Scenes].CreateScene(
-      engine,
-      canvasRef.current
-    );
+    if (!sceneName || !(sceneName in Scenes)) {
+      console.error(`Scene "${sceneName}" not found in Scenes.`);
+      return;
+    }
+
+    let scene;
+    try {
+      // Charger la scène dynamiquement
+      scene = Scenes[sceneName as keyof typeof Scenes].CreateScene(
+        engine,
+        canvasRef.current,
+      );
+    } catch (error) {
+      console.error("Error creating scene:", error);
+      engine.dispose();
+      return;
+    }
 
     // Lancer la boucle de rendu
     engine.runRenderLoop(() => {
       scene.render();
+    });
+
+    // hide/show the Inspector
+    window.addEventListener("keydown", (ev) => {
+      // Shift+Ctrl+Alt+I
+      if (ev.shiftKey && ev.ctrlKey && ev.altKey && ev.key === "I") {
+        if (scene.debugLayer.isVisible()) {
+          scene.debugLayer.hide();
+        } else {
+          scene.debugLayer.show();
+        }
+      }
     });
 
     // Ajuster la taille du canvas en cas de redimensionnement de la fenêtre
@@ -42,6 +68,12 @@ export default function RenderScene(): JSX.Element {
   }, [sceneName]);
 
   return (
-    <canvas id="renderCanvas" ref={canvasRef} className="w-full h-full block" />
+    <div>
+      <canvas
+        id="renderCanvas"
+        ref={canvasRef}
+        className="w-full h-full block"
+      />
+    </div>
   );
 }
