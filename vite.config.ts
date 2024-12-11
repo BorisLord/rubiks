a53351d5e45eb9de0e5af456d676b744564c563d
BorisPad <borisdelord@proton.me>
2024-12-11 21:16:28 +0100

    update tailwind v4

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import preact from "@preact/preset-vite";
 import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     outDir: "dist",
   },
-  plugins: [preact()],
+  plugins: [preact(), tailwindcss()],
   base: "/rubiks/",
   server: {
     hmr: {
